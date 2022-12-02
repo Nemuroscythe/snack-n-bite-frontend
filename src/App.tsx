4fa97055a351914d8e97cb717c1a7e1e847ebf91
Nemuroscythe <58225415+Nemuroscythe@users.noreply.github.com>
2022-12-02 15:20:01 +0100

    SB 10 ajout d'une route /menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Hello from "./component/Hello";
+import Menu from "./component/Menu";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Hello />}></Route>
+          <Route path="/menu" element={<Menu />}></Route>
         </Routes>
       </BrowserRouter>
     </div>

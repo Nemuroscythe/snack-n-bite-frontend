4dc8f791d4c670975a78ffa973ab8624634bb834
Nemuroscythe <58225415+Nemuroscythe@users.noreply.github.com>
2022-12-19 22:08:26 +0100

    SB10 Extra ajout d'un message d'erreur si unit price négatif ou 0

--- src/component/Menu.tsx
+++ src/component/Menu.tsx
@@ -13,14 +13,15 @@ export default function Menu() {
   }, []);
 
   const onChangeHandler = (event) => {
-    let {value, name} = event.target;
-    console.warn(event.target)
-    if(name == "ingredients"){
+    let { value, name } = event.target;
+    console.warn(event.target);
+    if (name == "ingredients") {
       name = name.split(",");
     }
-    setSelectedDish({...selectedDish, [name]: value});
+    validateField(name, value);
+    setSelectedDish({ ...selectedDish, [name]: value });
     console.log(selectedDish);
-  }
+  };
 
   function selectHandler(dish: Dish) {
     console.debug(Object.entries(dish));
@@ -28,35 +29,35 @@ export default function Menu() {
   }
 
   const createHandler = (event) => {
-    event.preventDefault();
-    let newDishes : Dish[] = [...dishes];
-    newDishes.push(new Dish(selectedDish?.id,
-      selectedDish?.name,
-      selectedDish?.unit_price))
+
+    let newDishes: Dish[] = [...dishes];
+    newDishes.push(
+      new Dish(selectedDish?.id, selectedDish?.name, selectedDish?.unit_price)
+    );
 
     createDish(selectedDish)
-      .then(() => console.log('Successfully created dish'))
+      .then(() => console.log("Successfully created dish"))
       .then(() => setDishes(newDishes));
-
-  }
+  };
 
   const updateHandler = (event) => {
     event.preventDefault();
-    let newDishes : Dish[] = [...dishes];
+
+    let newDishes: Dish[] = [...dishes];
     newDishes = newDishes.filter((dish) => {
       return dish.id != selectedDish.id;
     });
-    newDishes.push(new Dish(selectedDish?.id,
-      selectedDish?.name,
-      selectedDish?.unit_price))
+    newDishes.push(
+      new Dish(selectedDish?.id, selectedDish?.name, selectedDish?.unit_price)
+    );
 
     updateDish(selectedDish)
-      .then(() => console.log('Successfully updated dish'))
+      .then(() => console.log("Successfully updated dish"))
       .then(() => setDishes(newDishes));
-  }
+  };
 
-  const deleteHandler = ()=> {
-    let newDishes : Dish[] = [...dishes];
+  const deleteHandler = () => {
+    let newDishes: Dish[] = [...dishes];
     console.debug("newDishes : ");
     console.debug(newDishes);
 
@@ -67,6 +68,25 @@ export default function Menu() {
         })
       )
     );
+  };
+
+  function validateField(name: any, value: any) {
+    if (name == "unit_price") {
+      let unitPriceErrorMessage = document.getElementById("unit_price error");
+      let createButton = document.getElementById("createButton");
+      let updateButton = document.getElementById("updateButton");
+      const unitPriceError = "unit_price error";
+      if (value <= 0) {
+        console.warn("The unit price is negative or equal to 0.");
+        unitPriceErrorMessage.hidden = false;
+        updateButton.disabled = true;
+        createButton.disabled = true;
+        return;
+      }
+      unitPriceErrorMessage.hidden = true;
+      updateButton.disabled = false;
+      createButton.disabled = false;
+    }
   }
 
   return (
@@ -74,36 +94,94 @@ export default function Menu() {
       <div className="container">
         {dishes.map((dish, index) => (
           <div className="col">
-            <button key={index}  type="button" className="btn btn-light btn-block w-100" onClick={() => selectHandler(dish)}>
+            <button
+              key={index}
+              type="button"
+              className="btn btn-light btn-block w-100"
+              onClick={() => selectHandler(dish)}
+            >
               {dish.name}
             </button>
           </div>
         ))}
       </div>
       <div className="input-group mb-3">
-        <span className="input-group-text" id="basic-addon1">Nom </span>
-        <input type="text" className="form-control" placeholder="burger" value={selectedDish?.name} onChange={onChangeHandler} name="name"/>
+        <span className="input-group-text" id="basic-addon1">
+          Nom{" "}
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="burger"
+          value={selectedDish?.name}
+          onChange={onChangeHandler}
+          name="name"
+          required
+        />
       </div>
       <div className="input-group mb-3">
-        <span className="input-group-text" id="basic-addon1">Liste d'ingrédients </span>
-        <input type="text" className="form-control" placeholder="Bun, Onion, Meat patty" value={selectedDish?.ingredients} onChange={onChangeHandler} name="ingredients"/>
+        <span className="input-group-text" id="basic-addon1">
+          Liste d'ingrédients{" "}
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Bun, Onion, Meat patty"
+          value={selectedDish?.ingredients}
+          onChange={onChangeHandler}
+          name="ingredients"
+          required
+        />
       </div>
       <div className="input-group mb-3">
-        <span className="input-group-text" id="basic-addon1">Prix </span>
-        <input type="text" className="form-control" placeholder="10" value={selectedDish?.unit_price} onChange={onChangeHandler} name="unit_price"/>
+        <span className="input-group-text" id="basic-addon1">
+          Prix{" "}
+        </span>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="10"
+          value={selectedDish?.unit_price}
+          onChange={onChangeHandler}
+          name="unit_price"
+          required
+        />
         <span className="input-group-text">€</span>
+        <div
+          className="error my-3 alert alert-danger"
+          id="unit_price error"
+          hidden={true}
+        >
+          <em>Le prix doit être positif et non nul !</em>
+        </div>
       </div>
       <div className="dropdown">
-        <button className="btn btn-secondary" type="button" aria-expanded="false" onClick={createHandler}>
+        <button
+          className="btn btn-secondary"
+          id="createButton"
+          type="button"
+          aria-expanded="false"
+          onClick={createHandler}
+        >
           Créer
         </button>
-        <button className="btn btn-secondary" type="button" aria-expanded="false" onClick={updateHandler}>
+        <button
+          className="btn btn-secondary"
+          id="updateButton"
+          type="button"
+          aria-expanded="false"
+          onClick={updateHandler}
+        >
           Modifier
         </button>
-        <button className="btn btn-secondary" type="button" aria-expanded="false" onClick={deleteHandler}>
+        <button
+          className="btn btn-secondary"
+          type="button"
+          aria-expanded="false"
+          onClick={deleteHandler}
+        >
           Supprimer
         </button>
-        
       </div>
     </>
   );
